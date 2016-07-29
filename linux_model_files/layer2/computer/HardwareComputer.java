9628b2543a6f1849aaca55ec2f82a23205f3d988
raid-khan <khanraid@hotmail.com>
2016-07-29 09:54:24 +0200

    Create HardwareComputer.java

---
+++ linux_model_files/layer2/computer/HardwareComputer.java
@@ -0,0 +1,15 @@
+package se.kth.ics.pwnpr3d.layer2.computer;
+
+import se.kth.ics.pwnpr3d.layer0.Asset;
+
+public class HardwareComputer extends Computer {
+
+   public HardwareComputer(String name, Asset superAsset) {
+      super(name, superAsset);
+   }
+
+   public HardwareComputer(String name) {
+      super(name);
+   }
+
+}
