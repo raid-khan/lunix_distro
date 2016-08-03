f128e261b8ffcf7916d30350f96bd4feb043736f
raid-khan <khanraid@hotmail.com>
2016-08-03 14:57:21 +0200

    Create Constants.java

---
+++ linux_model_files/datatypes/Constants.java
@@ -0,0 +1,12 @@
+package se.kth.ics.pwnpr3d.datatypes;
+
+public class Constants {
+
+   public static final double oneSecond = 1 / (24 * 60 * 60);
+   public static final double infinity  = Double.MAX_VALUE;
+
+   public Constants() {
+      // TODO Auto-generated constructor stub
+   }
+
+}
