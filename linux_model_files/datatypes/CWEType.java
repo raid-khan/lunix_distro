ee0521f3868f1e54d6d40f3c4c8d3626f3221657
raid-khan <khanraid@hotmail.com>
2016-08-04 11:56:19 +0200

    Update CWEType.java

--- linux_model_files/datatypes/CWEType.java
+++ linux_model_files/datatypes/CWEType.java
@@ -1,5 +1,5 @@
 package se.kth.ics.pwnpr3d.datatypes;
 
 public enum CVEType {
-    CVE_79, CVE_89, NONE, CVE_119, HeartBleed, CVE_639, CVE_22, CVE_152, ShellShock
+    CVE_79, HeartBleed, ShellShock
 }
